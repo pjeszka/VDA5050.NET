18a832a2856c39c8be15008b404e1eba80a6b579
Pawel Jeszka <pjeszka@ess.etisoft.pl>
2026-01-24 20:09:39 +0100

    save

--- VDA5050.NET/Public/Models/Robots/RobotState.cs
+++ VDA5050.NET/Public/Models/Robots/RobotState.cs
@@ -10,7 +10,9 @@ public sealed record RobotState(
     BatteryState? BatteryState,
     bool IsDriving,
     bool? IsPaused,
-    SafetyState SafetyState)
+    bool? NewBaseRequest,
+    SafetyState SafetyState,
+    ICollection<Load>? Loads)
 {
     internal static RobotState FromMessage(StateMessage stateMessage)
     {
@@ -20,12 +22,107 @@ public sealed record RobotState(
             BatteryState.FromMessage(stateMessage.BatteryState),
             stateMessage.Driving,
             stateMessage.Paused,
-            SafetyState.FromMessage(stateMessage.SafetyState));
+            stateMessage.NewBaseRequest,
+            SafetyState.FromMessage(stateMessage.SafetyState),
+            stateMessage.Loads is null ? null : stateMessage.Loads.Select(Load.FromMessage).ToList());
     }
+}
+
+public sealed record BoundingBoxReference
+{
+    private BoundingBoxReference(double x, double y, double z, double? theta)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+        Theta = theta;
+    }
+
+    internal static BoundingBoxReference? FromMessage(BoundingBoxReferenceMessage? message)
+    {
+        if (message == null)
+        {
+            return null;
+        }
+        
+        return new BoundingBoxReference(message.X, message.Y, message.Z, message.Theta);
+    }
+
+    public double X { get; }
+    public double Y { get; }
+    public double Z { get; }
+    public double? Theta { get; }
+}
+
+public sealed record LoadDimensions
+{
+    private LoadDimensions(double length, double width, double? height)
+    {
+        Length = length;
+        Width = width;
+        Height = height;
+    }
+    
+    internal static LoadDimensions? FromMessage(LoadDimensionsMessage? message)
+    {
+        if (message == null)
+        {
+            return null;
+        }
+
+        return new LoadDimensions(message.Length, message.Width, message.Height);
+    } 
+    public double Length { get; }
+    public double Width { get; }
+    public double? Height { get; }
+}
+
+public sealed record Load
+{
+    private Load(
+        string? loadId,
+        string? loadType,
+        string? loadPosition,
+        double? weight,
+        BoundingBoxReference? boundingBoxReference,
+        LoadDimensions? dimensions)
+    {
+        LoadId = loadId;
+        LoadType = loadType;
+        LoadPosition = loadPosition;
+        Weight = weight;
+        BoundingBoxReference = boundingBoxReference;
+        Dimensions = dimensions;
+    }
+
+    internal static Load FromMessage(LoadMessage message)
+    {
+        return new Load(
+            message.LoadId,
+            message.LoadType,
+            message.LoadPosition,
+            message.Weight,
+            BoundingBoxReference.FromMessage(message.BoundingBoxReference),
+            LoadDimensions.FromMessage(message.Dimensions)
+        );
+    }
+
+    public string? LoadId { get; } = string.Empty;
+    public string? LoadType { get; }
+    public string? LoadPosition { get; }
+    public double? Weight { get; }
+    public BoundingBoxReference? BoundingBoxReference { get; }
+    public LoadDimensions? Dimensions { get; }
 }
 
 public sealed record SafetyState
 {
+    private SafetyState(EmergencyStopType eStop, bool fieldViolation)
+    {
+        EStop = eStop;
+        FieldViolation = fieldViolation;
+    }
+
     internal static SafetyState FromMessage(SafetyStateMessage? message)
     {
         if (message == null)
@@ -33,8 +130,12 @@ public sealed record SafetyState
             return null;
         }
         
-        return new SafetyState();
+        return new SafetyState(message.EStop, message.FieldViolation);
     }
+    
+    public EmergencyStopType EStop { get; }
+    
+    public bool FieldViolation { get; }
 }
 
 public sealed record BatteryState
@@ -58,11 +159,11 @@ public sealed record BatteryState
             message.Reach);
     }
     
-    public double BatteryCharge { get; set; }
-    public bool Charging { get; set; }
-    public double? BatteryVoltage { get; set; }
-    public int? BatteryHealth { get; set; }
-    public uint? Reach { get; set; }
+    public double BatteryCharge { get; }
+    public bool Charging { get; }
+    public double? BatteryVoltage { get; }
+    public int? BatteryHealth { get; }
+    public uint? Reach { get; }
 }
 
 public sealed record RobotVelocities
@@ -87,9 +188,9 @@ public sealed record RobotVelocities
             velocityMessage.Omega);
     }
 
-    public double Vx { get; set; }
+    public double Vx { get; }
     
-    public double Vy { get; set; }
+    public double Vy { get; }
     
-    public double Omega { get; set; }
+    public double Omega { get; }
 }
